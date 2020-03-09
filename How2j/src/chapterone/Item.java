c0b19bd1faa09457cd3fb25693670c6d5b85980f
shawnfang <shawnfang07@gmail.com>
2020-03-09 17:32:59 +0800

    test

---
+++ How2j/src/chapterone/Item.java
@@ -0,0 +1,21 @@
+package chapterone;
+
+public class Item {
+    String name;
+    int price;
+     
+    public static void main(String[] args) {
+        Item potion = new Item();  
+        potion.name= "ÑªÆ¿";
+        potion.price =50;
+         
+        Item shoe = new Item();
+        shoe.name= "²ÝÐ¬";
+        shoe.price =300;
+         
+        Item sword = new Item();   
+        sword.name= "³¤½£";
+        sword.price =350;
+         
+    }
+}
