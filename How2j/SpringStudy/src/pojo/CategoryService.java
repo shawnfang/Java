6d2e66afd16fd96e422300399d171c65ef78a19e
fangxiao <shawnfang07@gmail.com>
2020-04-08 07:17:24 +0800

    20200407

---
+++ How2j/SpringStudy/src/pojo/CategoryService.java
@@ -0,0 +1,8 @@
+package pojo;
+import org.springframework.stereotype.Component;
+@Component("cs")
+public class CategoryService {
+    public void  doSomeService(){
+        System.out.println("doSomeService 1");
+    }
+}
