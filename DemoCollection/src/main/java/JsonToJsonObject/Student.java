e346c26abb93ea068ebed27c6bb884f795bea6ee
shawnfang <shawnfang07@gmail.com>
2020-04-15 18:30:32 +0800

    update

---
+++ DemoCollection/src/main/java/JsonToJsonObject/Student.java
@@ -0,0 +1,47 @@
+package JsonToJsonObject;
+
+public class Student {
+    // 姓名
+    private String name;
+    // 年龄
+    private String age;
+    // 住址
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", age=" + age + ", address=" + address + "]";
+    }
+
+    /*
+    public void sayHello(Book book){
+        System.out.println(book.getName());
+
+    }
+
+     */
+}
---
+++ DemoCollection/src/main/java/JsonToJsonObject/Test.java
@@ -0,0 +1,12 @@
+package JsonToJsonObject;
+import JsonToJsonObject.Student;
+import com.alibaba.fastjson.JSONObject;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
