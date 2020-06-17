6f608b64d908195e5c5810a9bcc99e7ba5608c04
shawnfang <shawnfang07@gmail.com>
2020-06-17 19:12:02 +0800

    update

--- DataStructuresAndAlgorithms/src/main/java/com/study/linklist/NewLinkList.java
+++ DataStructuresAndAlgorithms/src/main/java/com/study/linklist/NewLinkList.java
@@ -3,16 +3,86 @@ package com.study.linklist;
 import com.study.linearlist.AdtList;
 
 public class NewLinkList<T> implements AdtList<T>,LinkListInterface<T> {
+    private ImproveNode firstNode;
+    private int length;
+    private ImproveNode lastNode;
+
+    public NewLinkList(){
+        firstNode = null;
+        length = 0;
+        lastNode = null;
+    }
+
+    private ImproveNode getNodeAt(int nodePosition){
+        ImproveNode improveNode = firstNode;
+        if (nodePosition<0 || nodePosition>length) {
+            return null;
+        }
+        if (isEmpty()) {
+            return null;
+        }
+        for (int i = 1; i < nodePosition; i++) {
+            improveNode = improveNode.getNextNode();
+        }
+        return improveNode;
+    }
+
     public boolean add(T newEntry) {
-        return false;
+        boolean result = true;
+        ImproveNode improveNode = new ImproveNode(newEntry);
+        if(isEmpty()){
+            firstNode = improveNode;
+            System.out.println("为空的时候添加成功");
+            System.out.println(firstNode.getData());
+        }else {
+            lastNode.setNextNode(improveNode);
+            System.out.println("添加成功");
+            System.out.println(lastNode.getData());
+        }
+        lastNode = improveNode;
+        length++;
+        return result;
     }
 
     public boolean add(int newPosition, T newEntry) {
-        return false;
+        boolean result = true;
+        ImproveNode improveNode = new ImproveNode(newEntry);
+        if (newPosition<0 || newPosition>length+1 || isEmpty()) {
+            result = false;
+            return result;
+        }
+        if (newPosition == 1) { // 插入头部
+            improveNode.setNextNode(firstNode);
+            firstNode = improveNode;
+        }else if(newPosition == length){  //插入末尾
+            lastNode.setNextNode(improveNode);
+            lastNode = improveNode;
+        } else { // 插入中间
+            ImproveNode frontImporveNode = getNodeAt(newPosition-1);
+            ImproveNode backImproveNode = getNodeAt(newPosition);
+            frontImporveNode.setNextNode(improveNode);
+            improveNode.setNextNode(backImproveNode);
+        }
+        length++;
+        return result;
     }
 
     public Object remove(int givePosition) {
-        return null;
+        T result = null;
+        if (givePosition == 1) {
+            firstNode = getNodeAt(givePosition+1);
+            result = (T)firstNode.getData();
+        }else if(givePosition>1 || givePosition<length){
+            ImproveNode beforeNode = getNodeAt(givePosition-1);
+            ImproveNode backNode = getNodeAt(givePosition+1);
+            beforeNode.setNextNode(backNode);
+            result = (T)getNodeAt(givePosition).getData();
+        }else {
+            result = (T)lastNode.getData();
+            ImproveNode beforeNode = getNodeAt(givePosition-1);
+            lastNode = beforeNode;
+        }
+        return result;
     }
 
     public void clean() {
@@ -20,23 +90,35 @@ public class NewLinkList<T> implements AdtList<T>,LinkListInterface<T> {
     }
 
     public boolean replace(int givenPosition, T newEntry) {
-        return false;
+        boolean replaceResult = true;
+        if (givenPosition<1 || givenPosition>length) {
+            replaceResult = false;
+        }else {
+            getNodeAt(givenPosition).setData(newEntry);
+        }
+        return replaceResult;
     }
 
     public Object getEntry(int givenPosition) {
-        return null;
+        return (T)getNodeAt(givenPosition).getData();
     }
 
     public boolean contains(T anEntry) {
-        return false;
+        boolean result = false;
+        for (int i = 0; i < length; i++) {
+            if (getEntry(i).equals(anEntry)) {
+               result = true;
+            }
+        }
+        return result;
     }
 
     public int getSize() {
-        return 0;
+        return length;
     }
 
     public boolean isEmpty() {
-        return false;
+        return length == 0;
     }
 
     public boolean isFull() {
@@ -44,7 +126,11 @@ public class NewLinkList<T> implements AdtList<T>,LinkListInterface<T> {
     }
 
     public void display() {
-
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 1; i <= length ; i++) {
+            stringBuffer.append(getNodeAt(i).getData()+" ");
+        }
+        System.out.println(stringBuffer);
     }
 
     public void addFirst(Object newEntry) {
@@ -73,5 +159,22 @@ public class NewLinkList<T> implements AdtList<T>,LinkListInterface<T> {
 
     public void moveToEnd() {
 
+    }
+
+    public static void main(String[] args) {
+        NewLinkList<String> newLinkList = new NewLinkList<String>();
+        newLinkList.add("a");
+        newLinkList.add("b");
+        newLinkList.add("c");
+        System.out.println(newLinkList.length);
+        newLinkList.display();
+        System.out.println(newLinkList.getEntry(1));
+        System.out.println("插入数据");
+        newLinkList.add(4,"x");
+        newLinkList.display();
+        System.out.println("替换");
+        newLinkList.replace(2,"tt");
+        newLinkList.display();
+        System.out.println(newLinkList.contains("as"));
     }
 }
