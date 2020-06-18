9cdeaec548f23119ef3573e55ad3c52b9290ad1b
shawnfang <shawnfang07@gmail.com>
2020-06-18 11:13:54 +0800

    update

--- DataStructuresAndAlgorithms/src/main/java/com/study/linklist/NewLinkList.java
+++ DataStructuresAndAlgorithms/src/main/java/com/study/linklist/NewLinkList.java
@@ -47,14 +47,14 @@ public class NewLinkList<T> implements AdtList<T>,LinkListInterface<T> {
     public boolean add(int newPosition, T newEntry) {
         boolean result = true;
         ImproveNode improveNode = new ImproveNode(newEntry);
-        if (newPosition<0 || newPosition>length+1 || isEmpty()) {
+        if (newPosition<0 || newPosition>length+2 || isEmpty()) {
             result = false;
             return result;
         }
         if (newPosition == 1) { // 插入头部
             improveNode.setNextNode(firstNode);
             firstNode = improveNode;
-        }else if(newPosition == length){  //插入末尾
+        }else if(newPosition == length+1){  //插入末尾
             lastNode.setNextNode(improveNode);
             lastNode = improveNode;
         } else { // 插入中间
@@ -134,30 +134,60 @@ public class NewLinkList<T> implements AdtList<T>,LinkListInterface<T> {
     }
 
     public void addFirst(Object newEntry) {
-
+        ImproveNode improveNode = new ImproveNode(newEntry);
+        improveNode.setNextNode(firstNode);
+        firstNode = improveNode;
+        length++;
     }
 
     public void addLast(Object newEntry) {
-
+        ImproveNode improveNode = new ImproveNode(newEntry);
+        lastNode.setNextNode(improveNode);
+        lastNode = improveNode;
+        length++;
     }
 
     public Object removeFirst() {
-        return null;
+        T nodeData = null;
+        nodeData = (T)firstNode.getData();
+        firstNode = firstNode.getNextNode();
+        length--;
+        return nodeData;
     }
 
     public Object removeLast() {
-        return null;
+        T nodeData = (T)lastNode.getData();
+        lastNode = getNodeAt(getSize()-1);
+        length--;
+        return nodeData;
     }
 
     public Object getFirst() {
-        return null;
+        T firstNodeData = null;
+        if (length == 0) {
+            return firstNodeData;
+        }else {
+            firstNodeData = (T) firstNode.getData();
+        }
+        return firstNodeData;
     }
 
     public Object getLast() {
-        return null;
+        T lastNodeData = null;
+        if (lastNode == null) {
+            return lastNodeData;
+        }else {
+            lastNodeData = (T)lastNode.getData();
+        }
+        return lastNodeData;
     }
 
     public void moveToEnd() {
+        ImproveNode tempNode;
+        tempNode = firstNode;
+        firstNode = firstNode.getNextNode();
+        lastNode.setNextNode(tempNode);
+        lastNode = tempNode;
 
     }
 
@@ -170,11 +200,34 @@ public class NewLinkList<T> implements AdtList<T>,LinkListInterface<T> {
         newLinkList.display();
         System.out.println(newLinkList.getEntry(1));
         System.out.println("插入数据");
-        newLinkList.add(4,"x");
+        newLinkList.add(3,"x");
+        System.out.println(newLinkList.lastNode.getData());
         newLinkList.display();
         System.out.println("替换");
         newLinkList.replace(2,"tt");
         newLinkList.display();
         System.out.println(newLinkList.contains("as"));
+        System.out.println("设置头部节点");
+        newLinkList.addFirst("fxx");
+        System.out.println(newLinkList.lastNode.getData());
+        newLinkList.display();
+        System.out.println("设置尾部节点");
+        newLinkList.display();
+        newLinkList.addLast("tailxx");
+        System.out.println(newLinkList.getEntry(6));
+        System.out.println(newLinkList.length);
+        newLinkList.display();
+        System.out.println("移除首节点");
+        System.out.println(newLinkList.removeFirst());
+        newLinkList.display();
+        System.out.println("移除末尾节点");
+        System.out.println(newLinkList.removeLast());
+        newLinkList.display();
+        System.out.println(newLinkList.lastNode.getData());
+        System.out.println("首节点移动到末尾");
+        newLinkList.moveToEnd();
+        newLinkList.display();
+        System.out.println("显示末尾节点是否正确");
+        System.out.println(newLinkList.lastNode.getData());
     }
 }
