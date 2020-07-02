779805a08dcad93d0783c18e5bd8268d4d679252
shawnfang <shawnfang07@gmail.com>
2020-07-02 18:08:16 +0800

    update

--- DataStructuresAndAlgorithms/src/main/java/com/study/stack/StackByLinkList.java
+++ DataStructuresAndAlgorithms/src/main/java/com/study/stack/StackByLinkList.java
@@ -32,34 +32,65 @@ public class StackByLinkList<T> implements StackInterface<T> {
     }
 
     private Node top = null;
-
+    private int count = 0;
     public boolean cleanStack() {
         return false;
     }
 
     public boolean popStack() {
-        return false;
+        boolean popStackResult = true;
+        if (emptyStack()) {
+            popStackResult = false;
+        }else {
+            top.data = null;
+            top = top.next;
+            count--;
+        }
+        return popStackResult;
     }
 
     public boolean pushStack(T newEntry) {
-        top.data = newEntry;
-
-        return false;
+        Node node = new Node(newEntry);
+        node.next = top;
+        top = node;
+        count++;
+        return true;
     }
 
     public T getTopStack() {
-        return null;
+        T data = null;
+        if (top != null) {
+            data = (T) top.data;
+        }
+        return data;
     }
 
     public boolean emptyStack() {
-        return false;
+        boolean emptyStackResult = false;
+        if (top == null) {
+            emptyStackResult = true;
+        }
+        return emptyStackResult;
     }
 
     public int getLengthStack() {
-        return 0;
+        return count;
     }
 
     public boolean destroyStack() {
         return false;
+    }
+
+    public static void main(String[] args) {
+        StackByLinkList<Integer> stackByLinkList = new StackByLinkList<Integer>();
+        stackByLinkList.pushStack(1);
+        stackByLinkList.pushStack(2);
+        stackByLinkList.pushStack(3);
+        System.out.println(stackByLinkList.getTopStack());
+        System.out.println(stackByLinkList.top.next.data);
+        stackByLinkList.pushStack(8);
+        stackByLinkList.pushStack(5);
+        stackByLinkList.popStack();
+        System.out.println(stackByLinkList.getTopStack());
     }
 }
